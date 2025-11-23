fee76898021015e14690f5ce542c3cbbb25a1b29
unknown <Dianaflochacon456@gmail.com>
2025-11-23 16:04:57 -0400

    modificado para ruta prueba

--- app.js
+++ app.js
@@ -159,9 +159,10 @@ app.use((err, req, res, next) => {
 
 // Iniciar servidor
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Servidor levantado en puerto " + PORT);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
+
 // app.listen(PORT, () => {
 //   console.log(`Servidor corriendo en el puerto ${PORT}`);
 // });
